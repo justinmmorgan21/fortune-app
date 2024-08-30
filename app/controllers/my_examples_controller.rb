9ddfd4251a8a51ef6bbed0fd435addc479bff6ba
justinmmorgan21 <justinmorgan21@gmail.com>
2024-08-29 19:26:02 -0500

    add 4 routes

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,44 @@
 class MyExamplesController < ApplicationController
+  def initialize
+    @visits = 0
+  end
+
+  def fortune_teller
+    # Feature 1: The page should return a random fortune. Every time you refresh the page, you’ll get a new fortune. You should make up at least 3 different fortunes.
+    fortune = ""
+    rand_num = rand(0..2)
+    fortune = "You will win the lottery this year" if rand_num == 0
+    fortune = "You will receive a promotion this year" if rand_num == 1
+    fortune = "You will meet the love of your life this year" if rand_num == 2
+
+    render json: {message: "Fortune: #{fortune}" }
+  end
+
+  def random_numbers
+    # Feature 2: The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
+    nums = []
+    (1..6).each { 
+      nums << rand(1..60)
+    }
+
+    render json: {message: "Lotto Numbers: #{nums}" }
+  end
+
+  def count_visits
+    # Feature 3: Every time a user visits/refreshes the page, the page should tell the user the number of visits made to that page so far. NOTE: This is impossible, but you should try your hand at this for a few minutes to discover why it’s impossible.
+    @visits += 1
+    object = self.to_s
+
+    render json: {message: "Number of visits: #{@visits}, object: #{object}"}
+  end
+
+  def bottles_o_beer
+    str = ""
+    arr = 99.step(by: -1, to: 3).entries
+    arr.each { |num|
+      str += "#{num} bottles of beer on the wall, #{num} bottles of beer. Take one down, pass it around, #{num - 1} bottles of beer on the wall... "
+    }
+    str += "2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall... 1 bottle of beer on the wall, 1 bottle of beer, take it down, pass it around, no more bottles of beer on the wall. :( :( :("
+    render json: {message: "#{str}"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "/fortune", controller: "my_examples", action: "fortune_teller"
+  
+  get "/lottery", controller: "my_examples", action: "random_numbers"
+  
+  get "/visits", controller: "my_examples", action: "count_visits"
+
+  get "/bottles", controller: "my_examples", action: "bottles_o_beer"
   # Defines the root path route ("/")
   # root "posts#index"
 end
